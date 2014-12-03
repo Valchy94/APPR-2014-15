ee6f390ee57aa4b8d52ffb851467c29091f6a11c
Valentina Gnamus <valentina.gnamus@gmail.com>
2014-12-03 23:09:56 +0100

    Dopolnjevanje podatkov in urejanje datoteke uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,26 @@
-# 2. faza: Uvoz podatkov
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
-
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+# # 2. faza: Uvoz podatkov
+# 
+# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
+# uvoziDruzine <- function() {
+#   return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
+#                       row.names = 1,
+#                       col.names = c("obcina", "en", "dva", "tri", "stiri"),
+#                       fileEncoding = "Windows-1250"))
+# }
+# 
+# # Zapišimo podatke v razpredelnico druzine.
+# cat("Uvažam podatke o družinah...\n")
+# druzine <- uvoziDruzine()
+# 
+# # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
+# # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
+# # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
+# # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
+# # fazah.
+uvoziTabelo_cas <- function() {
+     return(read.table("podatki/index_cas.csv", header=TRUE, sep = ";", 
+                       as.is = TRUE, row.names = 1, 
+                       fileEncoding = "Windows-1250"))
+   }
+cat("Uvažam podatke o spolni enakopravnosti glede na preživljanje prostega časa.\n")
+index_cas <- uvoziTabelo_cas()

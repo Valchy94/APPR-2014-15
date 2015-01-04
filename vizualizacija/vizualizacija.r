75e44410a3f7c406b656588f8b446c09f37c4cd1
Valentina Gnamus <valentina.gnamus@gmail.com>
2015-01-04 15:44:43 +0100

    Narisani zemljevidi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -14,9 +14,77 @@ drzave <- c('Belgium','Bulgaria','Czech Republic','Denmark', 'Germany','Estonia'
             'Slovenia','Slovakia','Finland','Sweden','United Kingdom')
 drzave <- drzave[drzave %in% svet$name_long]
 EU <- svet[svet$name_long %in% drzave,]
+casa<-c(cas$Time[1:16],cas$Time[18:27])
+dela<-c(delo$Work[1:16],delo$Work[18:27])
 denarja<-c(denar$Money[1:16],denar$Money[18:27])
+moci<-c(moc$Power[1:16],moc$Power[18:27])
+zdravja<-c(zdravje$Health[1:16],zdravje$Health[18:27])
+znanja<-c(znanje$Knowledge[1:16],znanje$Knowledge[18:27])
+EU$casmz <- casa
+EU$delomz<-dela
 EU$denarmz<-denarja
-spplot(EU, 'denarmz',col.regions=rainbow(26))
+EU$mocmz<-moci
+EU$zdravjemz<-zdravja
+EU$znanjemz<-znanja
+koordinate <- coordinates(EU)
+imena <- as.character(EU$name)
+rownames(koordinate) <- imena
+names(imena) <- imena
+koordinate["Cyprus",2] <- koordinate["Cyprus",2] - 1
+koordinate["United Kingdom",1] <- koordinate["United Kingdom",1]+1
+koordinate["United Kingdom",2] <- koordinate["United Kingdom",2]-1
+koordinate["Sweden",1] <- koordinate["Sweden",1]-1
+koordinate["Greece",1] <- koordinate["Greece",1]-0.8
+koordinate["Finland",2] <- koordinate["Finland",2]-2
+imena["United Kingdom"] <- "United\nKingdom"
+
+#Rišem zemljevide
+cat("Rišem zemljevide v PDF.\n")
+
+pdf("slike/zemljevid.pdf")
+print(spplot(EU, 'casmz', xlim=c(-25,40), ylim=c(33,73), 
+       main = "Enakopravnost spolov glede na prosti čas",
+       col.regions=rainbow(26, start=0, end=3/7),
+       sp.layout=list(
+         list("sp.text", koordinate, imena, cex=0.6)),
+       par.settings = list(panel.background=list(col="#FDFD99"))))
+
+print(spplot(EU, 'delomz', xlim=c(-25,40), ylim=c(33,73), 
+             main = "Enakopravnost spolov glede na delo",
+             col.regions=rainbow(26, start=0, end=3/7),
+             sp.layout=list(
+               list("sp.text", koordinate, imena, cex=0.6)),
+             par.settings = list(panel.background=list(col="#FDFD99"))))
+
+print(spplot(EU, 'denarmz', xlim=c(-25,40), ylim=c(33,73), 
+             main = "Enakopravnost spolov glede na prihodke",
+             col.regions=rainbow(26, start=0, end=3/7),
+             sp.layout=list(
+               list("sp.text", koordinate, imena, cex=0.6)),
+             par.settings = list(panel.background=list(col="#FDFD99"))))
+
+print(spplot(EU, 'mocmz', xlim=c(-25,40), ylim=c(33,73), 
+             main = "Enakopravnost spolov glede na moč",
+             col.regions=rainbow(26, start=0, end=3/7),
+             sp.layout=list(
+               list("sp.text", koordinate, imena, cex=0.6)),
+             par.settings = list(panel.background=list(col="#FDFD99"))))
+
+print(spplot(EU, 'zdravjemz', xlim=c(-25,40), ylim=c(33,73), 
+             main = "Enakopravnost spolov glede na zdravje",
+             col.regions=rainbow(26, start=0, end=3/7),
+             sp.layout=list(
+               list("sp.text", koordinate, imena, cex=0.6)),
+             par.settings = list(panel.background=list(col="#FDFD99"))))
+
+print(spplot(EU, 'znanjemz', xlim=c(-25,40), ylim=c(33,73), 
+             main = "Enakopravnost spolov glede na znanje",
+             col.regions=rainbow(26, start=0, end=3/7),
+             sp.layout=list(
+               list("sp.text", koordinate, imena, cex=0.6)),
+             par.settings = list(panel.background=list(col="#FDFD99"))))
+dev.off()
+
 
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
 # preuredi <- function(podatki, zemljevid) {

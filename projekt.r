8a73766e656cfca5499241047a1c1bb6e94af9c9
Valentina Gnamus <valentina.gnamus@gmail.com>
2015-01-04 16:01:16 +0100

    3.faza

--- projekt.r
+++ projekt.r
@@ -1,8 +1,8 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r", encoding="UTF-8")
 source("slike/Grafi.R",encoding="UTF-8")
-# # 3. faza: Analiza in vizualizacija podatkov
-# source("vizualizacija/vizualizacija.r")
+ # 3. faza: Analiza in vizualizacija podatkov
+source("vizualizacija/vizualizacija.r")
 # 
 # # 4. faza: Napredna analiza podatkov
 # source("analiza/analiza.r")
